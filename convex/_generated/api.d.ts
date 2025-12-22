4c01c938d9649b9843c0ad395508a9f007692d37
nostreet123 <guncea53@gmail.com>
2025-12-22 18:41:18 +0200

    Add seed data for reference tables

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,13 +8,17 @@
  * @module
  */
 
+import type * as seed from "../seed.js";
+
 import type {
   ApiFromModules,
   FilterApi,
   FunctionReference,
 } from "convex/server";
 
-declare const fullApi: ApiFromModules<{}>;
+declare const fullApi: ApiFromModules<{
+  seed: typeof seed;
+}>;
 
 /**
  * A utility for referencing Convex functions in your app's public API.
